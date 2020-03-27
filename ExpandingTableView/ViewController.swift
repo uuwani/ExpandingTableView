119b7a48632fb927d8102fe687b3d302ed338980
uuwani <paulchoi9502@gmail.com>
2020-03-27 19:01:24 +0900

    Initial Commit

---
+++ ExpandingTableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExpandingTableView
+//
+//  Created by Wanni on 2020/03/27.
+//  Copyright © 2020 Wanni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ExpandingTableViewTests/ExpandingTableViewTests.swift
@@ -0,0 +1,34 @@
+//
+//  ExpandingTableViewTests.swift
+//  ExpandingTableViewTests
+//
+//  Created by Wanni on 2020/03/27.
+//  Copyright © 2020 Wanni. All rights reserved.
+//
+
+import XCTest
+@testable import ExpandingTableView
+
+class ExpandingTableViewTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
